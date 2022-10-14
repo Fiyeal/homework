701c3be3f1e65715e20a2b165f7942587cffcd32
Fiyeal <fiyeal@163.com>
2022-10-14 13:44:27 +0800

    消除无用符号完成

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cassert>
 #include <fstream>
+#include <iterator>
 #include <set>
 #include <string>
 #include <cctype>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 // DEBUG FUNCTION
 template<class T>
@@ -18,6 +20,13 @@ void print(T a)
     return;
 }
 
+void print_map(std::map<char, std::vector<std::string>> a)
+{
+    for (auto it : a) {
+        std::cout << it.first << "->";
+        print(it.second);
+    }
+}
 
 ///////////////////////////////////////////
 
@@ -38,35 +47,65 @@ char start = 'S';
  */
 void to_chomsky(std::fstream &fin, std::fstream &fout)
 {
-    std::string s;
-    while (fin >> s) {
+    std::string line;
+    while (fin >> line) {
 
-        for (int i = 0; i < s.size(); ++i) { // 构造非终结符集V和终结符集T
-            if (i != 1 && i != 2 && s[i] != '|') {
-                if (std::isupper(s[i]))
-                    V.insert(s[i]);
+        for (int i = 0; i < line.size(); ++i) { // 构造非终结符集V和终结符集T
+            if (i != 1 && i != 2 && line[i] != '|') {
+                if (std::isupper(line[i]))
+                    V.insert(line[i]);
                 else
-                    T.insert(s[i]);
+                    T.insert(line[i]);
             }
         }
 
         // 拆分，例如'S->aABC|a' 变为 'S aABC a' ，存储
-        P.insert({s[0], std::vector<std::string>()});
+        P.insert({line[0], std::vector<std::string>()});
         int preind = 3, ind = 3;
-        std::cout << s << std::endl;
-        while ((ind = s.find_first_of("|", ind, s.size() - ind)) != std::string::npos) {
-            P[s[0]].push_back(std::string(s, preind, ind - preind));
+        std::cout << line << std::endl;
+        while ((ind = line.find_first_of("|", ind, line.size() - ind)) != std::string::npos) {
+            P[line[0]].push_back(std::string(line, preind, ind - preind));
             preind = ++ind;
         }
-        P[s[0]].push_back(std::string(s, preind, s.size() - preind));
-        for (auto val : P[s[0]]) {
+        P[line[0]].push_back(std::string(line, preind, line.size() - preind));
+        for (auto val : P[line[0]]) {
             std::cout << val << " ";
         }
         std::cout << std::endl << std::endl;
     }
 
     // 消除无用符号
+    std::set<char> checked, unchecked{'S'};
+    while (!unchecked.empty()) {
+        char check = 0;
+        if (unchecked.count('S') == 1) {
+            check = 'S';
+        }
+        else {
+            auto it = unchecked.begin();
+            check = *it;
+        }
+        for (auto s : P[check]) {
+            for (auto ch : s) {
+                if (std::isupper(ch) && checked.count(ch) == 0) {
+                    unchecked.insert(ch);
+                }
+            }
+        }
+        checked.insert(check);
+        auto it = unchecked.find(check);
+        unchecked.erase(it);
+    }
+    std::vector<char> tmp;
+    std::set_difference(V.begin(), V.end(), checked.begin(), checked.end(), std::back_inserter(tmp));
+    for (auto ch : tmp) {
+        auto it = P.find(ch);
+        P.erase(it);
+    }
+
+    // 消除单一产生式
     
+
 
     return;
 }
